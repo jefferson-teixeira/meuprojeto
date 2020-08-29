a6f826849fd19ce7ddb781771c89e9029d5452c7
Jefferson Teixeira <jeffersonads2019@gmail.com>
2020-08-29 12:57:53 -0300

    Second commit

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Enquete():
+    def __init__(self, enquete_id, texto="", data_publicacao=""):
+        self.enquete_id = enquete_id
+        self.texto = texto
+        self.data_publicacao = data_publicacao
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from models import Enquete
 # Create your views here.
 
 def bemvindo(request):
     return render(request, 'bemvindo.html')    
+
+
+def exibir(request, enquete_id):
+    enquetes = {
+        1: Enquete(1, 'Qual o seu nome?', '29/08/2020'),
+        2: Enquete(2, 'Qual sua idade?', '29/08/2020'),
+        3: Enquete(3, 'Qual sua cor favorita?', '29/08/2020')
+    }
+
+    enquete = enquetes[enquete_id]
+    
+    return render(request, 'enquete.html', {'enquete': enquete})
