e5c49cfc7716e028a502f8406b51b219340af141
Jefferson Teixeira <jeffersonads2019@gmail.com>
2020-09-05 14:54:41 -0300

    Reimplementation using models

--- enquetes/models.py
+++ enquetes/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
 # Create your models here.
 
-class Enquete():
-    def __init__(self, enquete_id, texto="", data_publicacao=""):
-        self.enquete_id = enquete_id
-        self.texto = texto
-        self.data_publicacao = data_publicacao
+class Enquete(models.Model):
+    texto = models.CharField(max_length=50)
+    data_publicacao = models.DateField()
--- enquetes/views.py
+++ enquetes/views.py
@@ -7,12 +7,5 @@ def bemvindo(request):
 
 
 def exibir(request, enquete_id):
-    enquetes = {
-        1: Enquete(1, 'Qual o seu nome?', '29/08/2020'),
-        2: Enquete(2, 'Qual sua idade?', '29/08/2020'),
-        3: Enquete(3, 'Qual sua cor favorita?', '29/08/2020')
-    }
-
-    enquete = enquetes[enquete_id]
-    
-    return render(request, 'enquete.html', {'enquete': enquete})
+    e = Enquete.objects.get(id=enquete_id)
+    return render(request, 'enquete.html', {'enquete': e})
